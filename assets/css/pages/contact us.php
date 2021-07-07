f526b998ce2122e30acde8a632c62350c148ecfc
marylyne-mutindi <marylynemutindi@gmail.com>
2021-07-07 12:04:44 +0300

    first commit

---
+++ assets/css/pages/contact us.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact us</title>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/WorkArea/FrameworkUI/css/ektron.stylesheet.ashx?id=-1759591071+-300771134+1985268503"><script async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.clarity.ms/scus/s/0.6.15/clarity.js"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/plugins/ua/ec.js"></script><script async="" src="https://www.clarity.ms/tag/45v40baouj"></script><script type="text/javascript" async="" src="https://snap.licdn.com/li.lms-analytics/insight.min.js"></script><script src="https://s.yimg.com/wi/ytc.js" async=""></script><script async="" src="//static.ads-twitter.com/uwt.js"></script><script type="text/javascript" async="" src="https://ssl.google-analytics.com/ga.js"></script><script src="https://s.yimg.com/wi/ytc.js" async=""></script><script src="https://s.yimg.com/wi/ytc.js" async=""></script><script src="https://s.yimg.com/wi/ytc.js" async=""></script><script src="https://connect.facebook.net/signals/config/660812384263044?v=2.9.43&amp;r=stable" async=""></script><script async="" src="https://connect.facebook.net/en_US/fbevents.js"></script><script src="https://s.yimg.com/wi/ytc.js" async=""></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-5RR62N2"></script><script type="text/javascript" src="/WorkArea/FrameworkUI/js/ektron.javascript.ashx?id=-569449246+-1939951303+-1080527330+-1687560804+-1388997516+2009761168+27274999+1979897163+-422906301+-1818005853+-1008700845+-1793043690"></script><title>Kenya Airways - The Pride of Africa</title>
+        <meta name="Description" content="Explore Our Great Selection of Destinations Around the World. Compare, Book and Manage your Kenya Airways flights. Check-in online and book a hotel for your trip.">
+        <meta name="Keywords" content="New Country Kenya">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="author" content="Kenya Airways">
+        <meta name="apple-itunes-app" content="app-id=1303500717">
+        <meta name="google-play-app" content="app-id=com.amadeus.merci.kq">
+        <link rel="apple-touch-icon" sizes="57x57" href="/static/images/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/static/images/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/static/images/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/static/images/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/static/images/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/static/images/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/static/images/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/static/images/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/images/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="/static/images/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/static/images/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/static/images/favicon/manifest.json">
+        <link rel="alternate" href="https://www.kenya-airways.com/" hreflang="x-default">
+        <meta name="msapplication-TileColor" content="#E42529">
+        <meta name="msapplication-TileImage" content="/static/images/favicon/ms-icon-144x144.png">
+        <link rel="preload" href="/static/css/styles.min.css" as="style">
+        <link rel="preload" href="/static/css/updates.min.css" as="style">
+        <meta name="theme-color" content="#E42529">
+        <link href="//cloud.typenetwork.com/projects/2773/fontface.css/" rel="stylesheet" type="text/css">
+        <link href="/static/css/styles.min.css" rel="stylesheet" type="text/css" async="" defer="">
+        <link href="/static/css/updates.css" rel="stylesheet" type="text/css" async="" defer="">
+        <script type="text/javascript"> dataLayer = [{'market': 'KE',
+        'language':'EN',
+        'page_type':'Landing Page'
+        t.createElement(a),s="dataLayer"!=n?"&l="+n:"";m.async=!0,m.src="https://www.googletagmanager.com/gtm.js?id="+r+s,g.parentNode.insertBefore(m,g)}(window,document,"script","dataLayer","GTM-5RR62N2");</script>
+        <script src="https://book.kenya-airways.com/plnext/kenyaairwaysDX/Preload.action?LANGUAGE=GB&amp;SITE=BASMBNEW" type="text/javascript"></script>
+    <head>
+        
+        
